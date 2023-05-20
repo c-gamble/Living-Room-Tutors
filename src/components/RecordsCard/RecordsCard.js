f2c5960b7d8cdfb66567a09840b26e7cf882c693
Cooper Gamble <gamble.cooper23@gmail.com>
2023-05-20 17:41:31 -0500

    logging subs

--- src/components/RecordsCard/RecordsCard.js
+++ src/components/RecordsCard/RecordsCard.js
@@ -137,14 +137,14 @@ function RecordsCard(props) {
   ];
   console.log(props.match)
   const subjects = [
-    { name: "K-5 Math ", status: props.match.tutor_K5_Math },
-    { name: "K-5 Reading ", status: props.match.tutor_K5_Reading },
+    { name: "K-5 Math ", status: props.match.tutor_k5_Math },
+    { name: "K-5 Reading ", status: props.match.tutor_k5_Reading },
     {
       name: "K-5 English/Writing ",
-      status: props.match.tutor_K5_English_Writing,
+      status: props.match.tutor_k5_English_Writing,
     },
-    { name: "K-5 Social Studies ", status: props.match.tutor_K5_Science },
-    { name: "K-5 Science ", status: props.match.tutor_K5_social_studies },
+    { name: "K-5 Social Studies ", status: props.match.tutor_k5_Science },
+    { name: "K-5 Science ", status: props.match.tutor_k5_social_studies },
     {
       name: "6-8th Language Arts ",
       status: props.match.tutor_6th_to_8th_language_arts,
